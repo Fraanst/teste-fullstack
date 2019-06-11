89aa2477eca7a557cc312534a60ee5bb39ae6081
Francine <Fran.stran@gmail.com>
2019-06-11 10:09:49 -0300

    Add BaseService

---
+++ Service/Services/ApplicationServiceBase.cs
@@ -0,0 +1,31 @@
+﻿using cv.Domain.Contracts;
+using cv.Domain.Contracts.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+	public class ApplicationServiceBase : NotificationServiceBase
+	{
+		private IUnitOfWork uow;
+		public ApplicationServiceBase(IUnitOfWork _uow, IDomainValidationProvider _notification) : base(_notification)
+		{
+			uow = _uow;
+		}
+		public bool Commit()
+		{
+			return uow.Commit();
+		}
+
+		public bool CommitDo(Action idCommitAction)
+		{
+			return uow.CommitDo(idCommitAction);
+		}
+
+		public void Dispose()
+		{
+			uow.Dispose();
+		}
+	}
+}
---
+++ cv.Domain/Contracts/Repository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cv.Domain.Contracts.Repository
+{
+	public interface IUnitOfWork
+	{
+		bool Commit();
+		bool CommitDo(Action ifCommitAction);
+		void Dispose();
+
+	}
+}
