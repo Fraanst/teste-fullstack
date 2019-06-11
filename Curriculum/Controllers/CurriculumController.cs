8c33dbdef904d4ea909e26cedc5623a50d557bb5
Francine <Fran.stran@gmail.com>
2019-06-11 10:07:30 -0300

    Add CurriculumController/CurriculumInterface

---
+++ Curriculum/Controllers/CurriculumController.cs
@@ -0,0 +1,48 @@
+﻿using cv.Domain.Contracts;
+using cv.Domain.Contracts.Services;
+using cv.Domain.ViewModels;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curriculum.Api.Controllers
+{
+	[Route("Curriculum")]
+	[ApiController]
+	public class CurriculumController : BaseController
+	{
+		ICurriculumService service;
+		public CurriculumController(IDomainValidationProvider _notification, ICurriculumService _service) : base(_notification)
+		{
+			service = _service;
+		}
+
+		[HttpGet("GetAll")]
+		public IActionResult GetAll()
+		{
+			return CreateResponse(service.GetAll());
+		}
+
+		[HttpGet("GetById")]
+		public IActionResult GetById([FromHeader]int id)
+		{
+			return CreateResponse(service.GetById(id));
+		}
+		[HttpPost("Create")]
+		public IActionResult Create([FromBody]CreateCurriculum dto)
+		{
+			return CreateResponse(service.Create(dto));
+		}
+
+		[HttpPut("Update")]
+		public IActionResult Update([FromBody]CreateCurriculum dto)
+		{
+			return CreateResponse(service.Update(dto));
+		}
+
+		[HttpPut("Delete")]
+		public IActionResult Delete([FromHeader]int id)
+		{
+			return CreateResponse(service.delete(id));
+		}
+	}
+}
---
+++ cv.Domain/Contracts/Services/ICurriculumService.cs
@@ -0,0 +1,17 @@
+﻿using cv.Domain.Entity;
+using cv.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cv.Domain.Contracts.Services
+{
+	public interface ICurriculumService
+	{
+		List<Curriculum> GetAll();
+		Curriculum GetById(int id);
+		bool Create(CreateCurriculum dto);
+		bool Update(CreateCurriculum dto);
+		bool delete(int id);
+	}
+}
