2118ec75ae5bdf8c5080db3e4cd624821f2c6b7d
Francine <Fran.stran@gmail.com>
2019-06-11 10:11:34 -0300

    Add Migrations

---
+++ DataInfrastructure/Migrations/20190611021216_initial.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataInfrastructure.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    CurriculumId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Curriculum",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    Resumo = table.Column<string>(nullable: true),
+                    End = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true),
+                    Interesses = table.Column<string>(nullable: true),
+                    Linkedin = table.Column<string>(nullable: true),
+                    SITE = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curriculum", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Curriculum_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Competencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CurriculumId = table.Column<int>(nullable: false),
+                    Competencia = table.Column<string>(nullable: true),
+                    Nivel = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competencias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Competencias_Curriculum_CurriculumId",
+                        column: x => x.CurriculumId,
+                        principalTable: "Curriculum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conquistas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CurriculumId = table.Column<int>(nullable: false),
+                    Conquista = table.Column<string>(nullable: true),
+                    Observacao = table.Column<string>(nullable: true),
+                    DateConquista = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conquistas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Conquistas_Curriculum_CurriculumId",
+                        column: x => x.CurriculumId,
+                        principalTable: "Curriculum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExperienciaProfissional",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CurriculumId = table.Column<int>(nullable: false),
+                    Empresa = table.Column<string>(nullable: true),
+                    Cargo = table.Column<string>(nullable: true),
+                    Atribuicoes = table.Column<string>(nullable: true),
+                    DateIn = table.Column<DateTime>(nullable: false),
+                    DateEnd = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExperienciaProfissional", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExperienciaProfissional_Curriculum_CurriculumId",
+                        column: x => x.CurriculumId,
+                        principalTable: "Curriculum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormacaoAcademica",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CurriculumId = table.Column<int>(nullable: false),
+                    Instituicao = table.Column<string>(nullable: true),
+                    Curso = table.Column<string>(nullable: true),
+                    Datein = table.Column<DateTime>(nullable: false),
+                    DateEnd = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormacaoAcademica", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormacaoAcademica_Curriculum_CurriculumId",
+                        column: x => x.CurriculumId,
+                        principalTable: "Curriculum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Competencias_CurriculumId",
+                table: "Competencias",
+                column: "CurriculumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conquistas_CurriculumId",
+                table: "Conquistas",
+                column: "CurriculumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Curriculum_UserId",
+                table: "Curriculum",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExperienciaProfissional_CurriculumId",
+                table: "ExperienciaProfissional",
+                column: "CurriculumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormacaoAcademica_CurriculumId",
+                table: "FormacaoAcademica",
+                column: "CurriculumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Competencias");
+
+            migrationBuilder.DropTable(
+                name: "Conquistas");
+
+            migrationBuilder.DropTable(
+                name: "ExperienciaProfissional");
+
+            migrationBuilder.DropTable(
+                name: "FormacaoAcademica");
+
+            migrationBuilder.DropTable(
+                name: "Curriculum");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
