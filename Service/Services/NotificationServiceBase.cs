e4e53bed1dcb60bbaffe8687c70bc86b94f4e824
Francine <Fran.stran@gmail.com>
2019-06-11 10:10:59 -0300

    Add NotificationBase

---
+++ Service/Services/NotificationServiceBase.cs
@@ -0,0 +1,28 @@
+﻿using cv.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Service.Services
+{
+	public class NotificationServiceBase
+	{
+		private readonly IDomainValidationProvider notification;
+
+		public NotificationServiceBase(IDomainValidationProvider _notification)
+		{
+			notification = _notification;
+		}
+
+		public void AddValidationError(Exception e, [CallerMemberName]string method = "")
+		{
+			notification.AddValidationError(e, method);
+		}
+
+		public void AddValidationError(string message, [CallerMemberName]string method = "")
+		{
+			notification.AddValidationError(message, method);
+		}
+	}
+}
