cbd02b5b73f3a06dc2367215da7025232d5af67f
Francine <Fran.stran@gmail.com>
2019-06-11 09:53:43 -0300

    add ConfigureConnectionString

---
+++ Curriculum/Startups/ConfigurationStartup.cs
@@ -0,0 +1,19 @@
+﻿
+using cv.Domain.ConfigurationService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Curriculum.Api.Startups
+{
+	public static class ConfigurationStartup
+	{
+		public static void ConfigurationRegister(this IServiceCollection services, IConfiguration Configuration)
+		{
+			ConectionStrings config = new ConectionStrings();
+			new ConfigureFromConfigurationOptions<ConectionStrings>(Configuration.GetSection("ConnectionStrings")).Configure(config);
+			services.AddSingleton(config);
+
+		}
+	}
+}
---
+++ cv.Domain/ConfigurationService/ConectionStrings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cv.Domain.ConfigurationService
+{
+	public class ConectionStrings
+	{
+		public string ConnString { get; set; }
+	}
+}
