86ce0217f5150d0837b2ade4fa1e61f9519b4082
Francine <Fran.stran@gmail.com>
2019-06-11 09:58:24 -0300

    Add Context

---
+++ DataInfrastructure/Context/CVContext.cs
@@ -0,0 +1,28 @@
+﻿
+using cv.Domain.Entity;
+using DataInfrastructure.Maps;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DataInfrastructure.Context
+{
+	public class CVContext : DbContext
+	{
+		public CVContext(DbContextOptions<CVContext> options)
+			: base(options)
+		{
+		}
+
+		public virtual DbSet<User> User { get; set; }
+		public virtual DbSet<Curriculum> Curriculum { get; set; }
+		public virtual DbSet<Conquistas> Conquistas { get; set; }
+		public virtual DbSet<FormacaoAcademica> FormacaoAcademica { get; set; }
+		public virtual DbSet<Competencias> Competencias { get; set; }
+		public virtual DbSet<ExperienciaProfissional> ExperienciaProfissional { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(BaseMap<BaseEntity>)));
+		}
+	}
+}
